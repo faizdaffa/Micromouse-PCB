1bc40f4aacde1009ee446fbe85372cb4562ec005
BiggusMaximus <annastyabagas164@gmail.com>
2023-06-13 02:41:47 +0700

    finishing adding pid

--- app/test.py
+++ app/test.py
@@ -1,102 +1,73 @@
-import sys
 import serial
+import time
 import argparse
-import numpy as np
-from time import sleep
-from collections import deque
-
+import pandas as pd
+import threading
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+parser = argparse.ArgumentParser(description='Script to handle boolean and list values.')
+
+# Add the boolean argument
+parser.add_argument('-pid', '--list', nargs='+', type=float, required=True)
+#parser.add_argument('-send', '--status', type=str, required=True)
+args = parser.parse_args()
+
+pid_values = args.list
+#spid_status = args.statusss
+
+serial_port = 'COM6' 
+baud_rate = 9600
+timeout = 5
+
+data_string = ','.join(str(value) for value in pid_values) + '\n'
+excel_file = {"Pitch":[]}
+
+print(data_string)
+fig, ax = plt.subplots()
+
+def send_data(ser, data):
+    ser.write(data.encode())
+
+def receive_data(ser):
+    timeout_duration = 1  # Duration of inactivity (in seconds) before program exit
+    last_receive_time = time.time()
+
+    while ser.is_open:
+        data = ser.readline().decode().strip()
+        if data:
+            print("Received:", data)
+            excel_file["Pitch"].append(data)
+            last_receive_time = time.time()
+
+        # Check if there has been enough inactivity to exit the program
+        if time.time() - last_receive_time > timeout_duration:
+            print("No data received for", timeout_duration, "seconds. Exiting program.")
+            
+            ser.close()
+            break
 
 
-# plot class
-class AnalogPlot:
-    # constr
-    def __init__(self, strPort, maxLen):
-        # open serial port
-        self.ser = serial.Serial(strPort, 9600)
-
-        self.ax = deque([0.0]*maxLen)
-        self.ay = deque([0.0]*maxLen)
-        self.maxLen = maxLen
-
-    # add to buffer
-    def addToBuf(self, buf, val):
-        if len(buf) < self.maxLen:
-            buf.append(val)
-        else:
-            buf.pop()
-            buf.appendleft(val)
-
-    # add data
-    def add(self, data):
-        assert (len(data) == 2)
-        self.addToBuf(self.ax, data[0])
-        self.addToBuf(self.ay, data[1])
-
-    # update plot
-    def update(self, frameNum, a0, a1):
-        try:
-            line = self.ser.readline()
-            print(line)
-            data = [float(val) for val in line.split()]
-            print(data)
-            # print data
-            if (len(data) == 2):
-                self.add(data)
-                a0.set_data(range(self.maxLen), self.ax)
-                a1.set_data(range(self.maxLen), self.ay)
-        except KeyboardInterrupt:
-            print('exiting')
-
-        return a0,
-
-    # clean up
-    def close(self):
-        # close serial
-        self.ser.flush()
-        self.ser.close()
-
-# main() function
 
 
-def main():
-    # create parser
-    parser = argparse.ArgumentParser(description="LDR serial")
-    # add expected arguments
-    parser.add_argument('--port', dest='port', required=True)
-
-    # parse args
-    args = parser.parse_args()
-
-    #strPort = '/dev/tty.usbserial-A7006Yqh'
-    strPort = args.port
-
-    print('reading from serial port %s...' % strPort)
-
-    # plot parameters
-    analogPlot = AnalogPlot(strPort, 100)
-
-    print('plotting data...')
-
-    # set up animation
-    fig = plt.figure()
-    ax = plt.axes(xlim=(0, 100), ylim=(-90, 90))
-    a0, = ax.plot([], [])
-    a1, = ax.plot([], [])
-    anim = animation.FuncAnimation(fig, analogPlot.update,
-                                   fargs=(a0, a1),
-                                   interval=50)
-
-    # show plot
-    plt.show()
-
-    # clean up
-    analogPlot.close()
-
-    print('exiting.')
-
-
-# call main
 if __name__ == '__main__':
-    main()
+    ser = serial.Serial(serial_port, baud_rate, timeout=timeout)
+    send_thread = threading.Thread(target=send_data, args=(ser, data_string,))
+    receive_thread = threading.Thread(target=receive_data, args=(ser,))
+    try:
+        send_thread.start()
+        time.sleep(0.5)
+        receive_thread.start()
+        
+    except serial.SerialException:
+        print("Error in serial communication")
+
+    send_thread.join()
+    receive_thread.join()
+    pd.DataFrame(excel_file).to_excel(f"Kp_{pid_values[0]}_Ki_{pid_values[1]}_Kd_{pid_values[2]}_.xlsx", index=False)
+    df = pd.read_excel(f"Kp_{pid_values[0]}_Ki_{pid_values[1]}_Kd_{pid_values[2]}_.xlsx")
+    ax.xaxis.set_major_locator(MultipleLocator(20))
+    ax.xaxis.set_major_formatter('{x:.0f}')
+    ax.xaxis.set_minor_locator(MultipleLocator(5))
+    ax.plot(df.index, df["Pitch"])
+    plt.savefig(f"Kp_{pid_values[0]}_Ki_{pid_values[1]}_Kd_{pid_values[2]}_.png")
+    plt.show()        
